ea9cac00824d494af4c970dcf5ee9aa54718f7a0
Melvyn Laïly <melvyn.laily@gmail.com>
2016-03-28 13:39:16 +0200

    Move the internal library code to separate folders

--- src/AudioSwitcherApi/Drawing/Internal/Interop/LookupIconIdFromDirectoryExFlags.cs
+++ src/AudioSwitcherApi/Drawing/Internal/Interop/LookupIconIdFromDirectoryExFlags.cs
@@ -5,7 +5,7 @@ using System;
 
 namespace AudioSwitcher.Drawing.Interop
 {
-    public enum LookupIconIdFromDirectoryExFlags : int
+    internal enum LookupIconIdFromDirectoryExFlags : int
     {
         LR_DEFAULTCOLOR = 0,
         LR_MONOCHROME = 1
